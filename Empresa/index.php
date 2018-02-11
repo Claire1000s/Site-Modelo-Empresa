b4bd410c1c61e3f3acd3bc680fe6627dbf3c17f1
Leticia Ferreira <claire_thousands@hotmail.com>
2018-02-10 23:26:49 -0200

    Add files via upload

---
+++ Empresa/index.php
@@ -0,0 +1,103 @@
+<?php
+    // put your code here
+?>
+
+<!DOCTYPE html>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+    </head>
+    <body>
+        <header>
+            <div id="banner">
+                <img id="logo" src="Images/logo.png" width="120" alt="Logo da Empresa" />
+            
+                <nav id="menu">
+                    <ul>
+                        <li><a href="#" target="_self">HOME</a></li>
+                        <li><a href="#" target="_self">QUEM SOMOS</a></li>
+                        <li><a href="#" target="_self">NOSSOS SERVIÇOS</a></li>
+                        <li><a href="#" target="_self">NOSSOS PARCEIROS</a></li>
+                        <li><a href="#" target="_self">CONTATO</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        
+        <div id="slider">
+            <img src="Images/imagem1.jpg" width="100%" alt="Slidershow da Empresa" />
+        </div>
+        
+        <section>
+            <h2 id="conteudo">Conteúdo</h2>
+            <p id="frase">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+            <div id="secao1">
+                <div class="quadrado">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type 
+                    specimen book. It has survived not only five centuries, but also the leap into 
+                    electronic typesetting, remaining essentially unchanged. It was popularised in 
+                    the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                    and more recently with desktop publishing software like Aldus PageMaker including 
+                    versions of Lorem Ipsum.It has survived not only five centuries, but also the leap 
+                    into electronic typesetting, remaining essentially unchanged. It was popularised in 
+                    the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and 
+                    more recently with desktop publishing software like Aldus PageMaker including versions 
+                    of Lorem Ipsum.</p>
+                </div>
+                
+                <div class="quadrado">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type 
+                    specimen book. It has survived not only five centuries, but also the leap into 
+                    electronic typesetting, remaining essentially unchanged. It was popularised in 
+                    the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                    and more recently with desktop publishing software like Aldus PageMaker including 
+                    versions of Lorem Ipsum.It has survived not only five centuries, but also the leap 
+                    into electronic typesetting, remaining essentially unchanged. It was popularised in 
+                    the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and 
+                    more recently with desktop publishing software like Aldus PageMaker including versions 
+                    of Lorem Ipsum.</p>
+                </div>
+                
+                <div class="quadrado">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type 
+                    specimen book. It has survived not only five centuries, but also the leap into 
+                    electronic typesetting, remaining essentially unchanged. It was popularised in 
+                    the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                    and more recently with desktop publishing software like Aldus PageMaker including 
+                    versions of Lorem Ipsum.It has survived not only five centuries, but also the leap 
+                    into electronic typesetting, remaining essentially unchanged. It was popularised in 
+                    the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and 
+                    more recently with desktop publishing software like Aldus PageMaker including versions 
+                    of Lorem Ipsum.</p>
+                </div>
+            </div>
+        </section>
+        
+        <section>
+            <div id="secao2">
+                <img id="grafico" src="Images/grafico.png" width="350" alt="Gráfico da Empresa" />
+                <p id="frase2">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </section>
+        
+        <footer>
+            <div id="rodape">
+                <p>Empresa 2018 - Todos os direitos reservados.</p>
+                <p>Design by Leticia Ferreira</p>
+            </div>
+        </footer>
+    </body>
+</html>
